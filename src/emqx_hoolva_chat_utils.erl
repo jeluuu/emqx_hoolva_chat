03bf0d71c6f47fe8b42c3ccfb896d500f6d7ba65
Abdul Jaleel <abduljaleelva@netstratum.com>
2022-10-23 22:05:23 +0530

    jsx ready next error checking proplist dont getting

--- src/emqx_hoolva_chat_utils.erl
+++ src/emqx_hoolva_chat_utils.erl
@@ -6,7 +6,7 @@ self_message(To, Message1, Message) ->
     io:format("reached self_message\n to : ~p~nmessage1 : ~p~nMessage :~p~n",[To,Message1,Message]),
     From = proplists:get_value(<<"from">>,Message),
     % Message1 = Message ++ [{<<"self">>, true}],
-    FinalMessage = {[{<<"data">>, Message1}]},
+    FinalMessage = {[{<<"data">>, Message}]},
     EncodedFinalMsg = jsx:encode(element(1,FinalMessage)),
     Publish = emqx_message:make(From, 2,To ,EncodedFinalMsg),
     emqx:publish(Publish).
