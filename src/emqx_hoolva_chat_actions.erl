516099148ca3cc5142ed06c045f39ea0f08c3eea
Abdul Jaleel <abduljaleelva@netstratum.com>
2022-10-24 12:24:43 +0530

    list - binary

--- src/emqx_hoolva_chat_actions.erl
+++ src/emqx_hoolva_chat_actions.erl
@@ -73,7 +73,7 @@ publish(Message) ->
                 [R] ->
 
                     io:format("~n already exist ~n"),
-                    From0 = [maps:get(<<"from">>,R)] ++ [From],
+                    From0 = [maps:get(<<"from_id">>,R)] ++ [From],
                     Message0 = [maps:get(<<"message">>,R)] ++ [Message1],
                     Date0 = [maps:get(<<"time">>,R)] ++ [Date],
                     io:format("~n added ~p --- ~p --- ~p ~n",[From0,Message0,Date0]),
