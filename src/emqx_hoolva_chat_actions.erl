63ead42c836f6748cd6078e5f2f459b3b403372f
Abdul Jaleel <abduljaleelva@netstratum.com>
2022-10-24 12:00:04 +0530

    list - binary

--- src/emqx_hoolva_chat_actions.erl
+++ src/emqx_hoolva_chat_actions.erl
@@ -69,13 +69,19 @@ publish(Message) ->
                     put_chat(ChatOutput),
                     P = get_chat(#{to_id => Topic}),
                     io:format("~n tivan ---- get_chat ~p ~n",[P]);
+                % [#{to_id := Topic, from_id := From0, message := Message0, time := Time0}] ->
                 [R] ->
 
                     io:format("~n already exist ~n"),
+                    From0 = [maps:get(<<"from">>,R)] ++ [From],
+                    Message0 = [maps:get(<<"message">>,R)] ++ [Message1],
+                    Date0 = [maps:get(<<"time">>,R)] ++ [Date],
+                    io:format("~n added ~p --- ~p --- ~p ~n",[From0,Message0,Date0]),
 
-                    ChatOutput1 = R#{from_id => From
-                        , message => Message1
-                        , time => Date
+                    ChatOutput1 = #{to_id => Topic
+                        , from_id => From0
+                        , message => Message0
+                        , time => Date0
                     },
                     put_chat(ChatOutput1),
                     P = get_chat(#{to_id => Topic}),
